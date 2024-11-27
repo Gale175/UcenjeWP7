7e5f0af3b3542abfac3a83479ff851c49d9efa21
Martin Galik <gale1508@gmail.com>
2024-11-27 20:57:04 +0100

    Kreiranje nove baze i tablica

---
+++ ispiti.sql
@@ -0,0 +1,22 @@
+﻿use master;
+go
+drop database if exists ispiti;
+go
+create database ispiti;
+go
+use ispiti;
+
+create table IspitniRok(
+sifra int,
+predmet varchar(30),
+vrstaIspita char(50),
+datum datetime,
+pristupio bit
+);
+
+create table Pristupnici(
+ispitniRok int,
+student char(80),
+brojBodova int,
+ocjena int
+);
