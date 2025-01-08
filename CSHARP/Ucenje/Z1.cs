ee04048c5e992aa74a6378e0d01e411165fea995
Martin Galik <gale1508@gmail.com>
2025-01-08 21:09:24 +0100

    2 zadatka

--- CSHARP/Ucenje/Z1.cs
+++ CSHARP/Ucenje/Z1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace Ucenje
 {
@@ -17,12 +18,34 @@ namespace Ucenje
 
 
             int d, s;
-            Console.Write("  Unesi duljinu stranice pravoktunika: ");
+            Console.Write(" Unesi duljinu stranice pravoktunika: ");
             d = int.Parse(Console.ReadLine());
-            Console.Write("  Unesi širinu stranice pravokutnika: ");
+            Console.Write(" Unesi širinu stranice pravokutnika: ");
             s = int.Parse(Console.ReadLine());
             Console.WriteLine("Površina pravokutnika iznosi:{0}", d * s);
 
+
+
+            Console.WriteLine("********************************");
+            Console.WriteLine("2.-VRSTA BROJA");
+
+            double c;
+            Console.Write(" Unesi broj: ");
+            c = Double.Parse(Console.ReadLine());
+
+            if (c == 0)
+            {
+
+                Console.WriteLine("Unešeni broj je 0.");
+
+
+            }
+            else 
+            { 
+            
+                Console.WriteLine("Unešeni broj je {0}!", c > 0 ? "pozitivan" : "negativan");
+            
+            }
 
         }
     }
