6a6d4ae420c4d8fd0382de32dafceffda0a3e017
Martin Galik <gale1508@gmail.com>
2025-02-05 20:49:43 +0100

    Nastavak rada sa WebApi

--- CSHARP/WebAPI9/Controllers/HttpMetodeController.cs
+++ CSHARP/WebAPI9/Controllers/HttpMetodeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI9.Models;
 
 namespace WebAPI9.Controllers
 {
@@ -10,15 +11,11 @@ namespace WebAPI9.Controllers
 
         // ovdje pocinje ruta 
         // ruta je metoda
-
         [HttpGet] // ovo je atribut koji govori da je ovo metoda za GET zahtjev
         public string HelloWorld() // metoda koja vraca string
         {
             return "Hello World!";
         }
-
-
-
         // ovdje zavrsava ruta
 
 
@@ -36,6 +33,58 @@ namespace WebAPI9.Controllers
         // ovdje zavrsava ruta
 
 
+        // ovdje pocinje ruta
+
+        [HttpGet]
+        [Route("json")] // ruta ce se zvati api/v1/HttpMetode/json
+
+        public IActionResult GetJson(string ime, int godina) // metoda koja vraca JSON
+        {
+            return Ok(new {Ime= ime, Prezime= "Doe", godina});
+        }
+        // ovdje završava ruta
+
+
+
+
+        // ovdje pocinje ruta
+        [HttpPost]
+        public IActionResult Post (Osoba osoba) // metoda koja prima JSON
+        {
+            osoba.Ime = "Hello " + osoba.Ime;
+            return StatusCode(StatusCodes.Status201Created, osoba); // vraca status 201 - StatusCode (201, osoba)
+        }
+        // ovdje zavrsava ruta
+
+
+        //ovdje pocinje ruta
+        [HttpPut]
+        public IActionResult Put(Osoba osoba)
+        {
+            osoba.Ime = "Promjenio " + osoba.Ime;
+            return Ok (osoba); 
+        }
+
+
+        //ovdje zavrsava ruta
+
+
+
+
+        // ovdje pocinje ruta
+        [HttpDelete]
+        public IActionResult Delete(int sifra)
+        {
+            if (sifra <= 0)
+            {
+                return BadRequest(new {poruka = "Sifra mora biti veca od 0" });
+            }
+            else
+            {
+                return Ok(new {poruka = "Obrisano" });
+            }
+        }
+        // ovdje zavrsava ruta
 
 
     }
